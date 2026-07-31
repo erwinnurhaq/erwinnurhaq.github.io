94b15a0aee15ff6731350cef19244a9a69ed3d54
Erwin Nurhaq <simkalastoforka@gmail.com>
2026-07-31 17:18:48 +0700

    refactor: replace markdown list layout with HTML, improve post styling, and update CSS variables and JS formatting

--- main.js
+++ main.js
@@ -4,45 +4,47 @@ const cursorOne = document.querySelector(".n-cursor-one");
 const cursorTwo = document.querySelector(".n-cursor-two");
 const footerYear = document.getElementById("footer-year");
 
-const hoverableTagName = ['A', 'H1', 'H2', 'H3']
+const hoverableTagName = ["A", "H1", "H2", "H3"];
 let mouseX = 0;
 let mouseY = 0;
 let hoverable = false;
 let needsUpdate = false;
 
 function mouseMove({ clientX, clientY, target }) {
-  mouseX = clientX;
-  mouseY = clientY;
-  hoverable = target.getAttribute("data-hoverable") !== null || hoverableTagName.includes(target.tagName);
-  needsUpdate = true;
+	mouseX = clientX;
+	mouseY = clientY;
+	hoverable =
+		target.getAttribute("data-hoverable") !== null ||
+		hoverableTagName.includes(target.tagName);
+	needsUpdate = true;
 }
 
 function mouseDown() {
-  cursorOne.classList.add("n-cursor-down");
+	cursorOne.classList.add("n-cursor-down");
 }
 
 function mousesUp() {
-  cursorOne.classList.remove("n-cursor-down");
+	cursorOne.classList.remove("n-cursor-down");
 }
 
 function updateCursor() {
-  if (needsUpdate) {
-    crosshairV.style.left = `${mouseX}px`;
-    crosshairH.style.top = `${mouseY}px`;
-    cursorOne.style.left = `${mouseX}px`;
-    cursorOne.style.top = `${mouseY}px`;
-    cursorTwo.style.transform = `translate3d(calc(${mouseX}px - 50%), calc(${mouseY}px - 50%), 0) rotate(45deg)`;
-    cursorTwo.style.borderWidth = hoverable ? `4px` : `1px`;
-    needsUpdate = false;
-  }
-  requestAnimationFrame(updateCursor);
+	if (needsUpdate) {
+		crosshairV.style.left = `${mouseX}px`;
+		crosshairH.style.top = `${mouseY}px`;
+		cursorOne.style.left = `${mouseX}px`;
+		cursorOne.style.top = `${mouseY}px`;
+		cursorTwo.style.transform = `translate3d(calc(${mouseX}px - 50%), calc(${mouseY}px - 50%), 0) rotate(45deg)`;
+		cursorTwo.style.borderWidth = hoverable ? `4px` : `1px`;
+		needsUpdate = false;
+	}
+	requestAnimationFrame(updateCursor);
 }
 
 window.addEventListener("mousemove", mouseMove);
 window.addEventListener("mousedown", mouseDown);
 window.addEventListener("mouseup", mousesUp);
 if (footerYear) {
-  footerYear.innerHTML = new Date().getFullYear();
+	footerYear.innerHTML = new Date().getFullYear();
 }
 console.info(`> Ich bin gut. lololol ∘ ∘ ∘ ( °ヮ° )' `);
 requestAnimationFrame(updateCursor);
